d65619df96679ad521912e3aa5869583b050f5b6
hacip <merthacip@gmail.com>
2013-01-20 20:26:44 +0200

    dfg

--- gui/test.py
+++ gui/test.py
@@ -1,1 +1,11 @@
-print "test"
+from espeak import espeak
+from datetime import datetime
+import time
+
+str = "this is an example text"
+print str
+stat = espeak.is_playing
+print stat
+espeak.synth(str)
+
+    
